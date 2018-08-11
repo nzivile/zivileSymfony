f6075b3ee1966c55ed7459eadd2f5aeeaa1e2c0b
Živilė Stankevičienė <n.zivile@yahoo.com>
2018-08-11 20:01:18 +0300

    Entity, Migration added.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -23,4 +25,22 @@ class ArticleController extends Controller
 
         return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
+
+//    /**
+//     * @Route("/article/save")
+//     */
+//    public function save(){
+//        $entityManager = $this->getDoctrine()->getManager();
+//
+//        $article = new Article();
+//        $article->setTitle('Article Two');
+//        $article->setBody('This is the body for article Two');
+//
+//        $entityManager->persist($article);
+//
+//        $entityManager->flush();
+//
+//        return new Response('Saved an article with the id of'.$article->getId());
+//    }
 }
+
